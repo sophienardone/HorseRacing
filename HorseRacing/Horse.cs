605c4087b1fcf03f2ad680a41c6a5e46e4680f89
sophie <d00259237@student.dkit.ie>
2024-10-20 12:05:35 +0100

    Addede Horse class with properties and constructor

---
+++ HorseRacing/Horse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRacing
+{
+    public class Horse
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string HorseId { get; set; }
+        public Horse(string name, DateTime dateOfBirth, string horseID) {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            HorseId = horseID;
+
+        }
+
+    }
+}
