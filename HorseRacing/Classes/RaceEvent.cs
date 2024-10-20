d7a0d5bf9d4acf6b6ca9b70281c70b4061af24e5
sophie <d00259237@student.dkit.ie>
2024-10-20 14:33:55 +0100

    Implemented RaceEvent class inheriting from Event with AddRace method and displayDetails override

---
+++ HorseRacing/Classes/RaceEvent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRacing.Classes
+{
+    public class RaceEvent : Event
+    {
+        public int RaceCount { get; set; }
+        public List<Race> Races { get; set; }
+
+        public RaceEvent(string eventName, string eventLocation, int raceCount) : base(eventName, eventLocation)
+        {
+            RaceCount = raceCount;
+            Races = new List<Race>();
+        }
+
+
+        public void AddRace(Race race)
+        {
+            Races.Add(race);
+            RaceCount = Races.Count;
+        }
+
+        public override void displayDetails()
+        {
+            Console.WriteLine($"Event: {eventName}, Location: {eventLocation}, Amount of Races: {RaceCount}");
+
+            foreach (var race in Races)
+            {
+                Console.WriteLine($"Race Name: {race.Name}, Start Time: {race.StartTime}");
+                foreach (var horse in race.Horses)
+                {
+                    Console.WriteLine($"Horse Name: {horse.Name}, Horse ID: {horse.HorseId}");
+                }
+            }
+        }
+    }
+
+   
+}
