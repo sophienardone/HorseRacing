9c8fff954df32f420ba8e6d943b4225a55eb35db
sophie <d00259237@student.dkit.ie>
2024-10-20 13:47:41 +0100

    Added Event abstract class with properties and displayDetails method

---
+++ HorseRacing/Classes/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRacing.Classes
+{
+    public abstract class Event
+    {
+        public string eventName {  get; set; }
+        public string eventLocation { get; set; }
+        public Event(string eventName, string eventLocation)
+        {
+            this.eventName = eventName;
+            this.eventLocation = eventLocation;
+
+        }
+
+        public abstract void displayDetails();
+    }
+}
