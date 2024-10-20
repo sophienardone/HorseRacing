8f15954f7fa67893439fa2890aba54045992c3c0
sophie <d00259237@student.dkit.ie>
2024-10-20 13:05:09 +0100

    Added Race class with properties, constructor, and AddHorse method

---
+++ HorseRacing/Classes/Race.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRacing.Classes
+{
+    public class Race
+    {
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public List<Horse> Horses { get; set; }
+
+        public Race(string name, DateTime startTime)
+        {
+            Name = name;
+            StartTime = startTime;
+            Horses = new List<Horse>();
+        }
+
+        public void AddHorse(Horse horse)
+        {
+            Horses.Add(horse);
+        }
+    }
+}
